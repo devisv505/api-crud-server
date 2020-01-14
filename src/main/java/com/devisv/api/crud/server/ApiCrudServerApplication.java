8ef7554f42c70778b6caccb5906030a3b97f9692
devisv505 <devisv505@gmail.com>
2020-01-14 12:35:48 +0300

    first commit

---
+++ src/main/java/com/devisv/api/crud/server/ApiCrudServerApplication.java
@@ -0,0 +1,13 @@
+package com.devisv.api.crud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiCrudServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/devisv/api/crud/server/ApiCrudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devisv.api.crud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
